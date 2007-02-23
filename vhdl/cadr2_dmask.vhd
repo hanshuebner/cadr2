e16d886c80cda3a94d4712c49f27806ba9cd00d1
rjs <rjs@09d5f3f9-6417-0410-bd0c-e187ee50e7ab>
2007-02-23 20:38:46 +0000

    Rename ports.
    git-svn-id: file:///Users/hans/cadr2-svn/trunk/cadr2@955 09d5f3f9-6417-0410-bd0c-e187ee50e7ab

--- vhdl/cadr2_dmask.vhd
+++ vhdl/cadr2_dmask.vhd
@@ -9,14 +9,14 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity cadr2_dmask is
-  port (addr : in std_logic_vector(4 downto 0);
+  port (a : in std_logic_vector(4 downto 0);
   	    do : out std_logic_vector(6 downto 0));
 end cadr2_dmask;
 
 architecture Behavioral of cadr2_dmask is
 
 begin
-  with addr select
+  with a select
   	do <= "0000000" when "00000",
 		   "0000001" when "00001",
 		   "0000011" when "00010",
